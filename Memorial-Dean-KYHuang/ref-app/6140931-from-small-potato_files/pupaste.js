51bea7ac8e3a4c5b01863de4f6fbdfc373546d53
twnanda <lsn46@mail.ncku.edu.tw>
2025-11-26 02:23:21 +0000

    Deploying to gh-pages from @ twnanda/Academia-and-Education@4bcb90adab3d228c097b1ba6273af5e0104ec536 🚀

--- Memorial-Dean-KYHuang/ref-app/6140931-from-small-potato_files/pupaste.js
+++
@@ -1,3 +0,0 @@
-function addUdnBookmark( f_title, f_url, f_digest, f_via) {
-void(open('http://bookmark.udn.com/add?f_TITLE='+encodeURIComponent(f_title)+'&f_URL='+encodeURIComponent(f_url)+'&f_DIGEST='+encodeURIComponent(f_digest)+'&via='+encodeURIComponent(f_via)));
-}
